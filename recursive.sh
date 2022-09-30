9ec350a10ddc5b3598b987cd5a2a62dcd72fa180
Karen Keegan <karen.keegan@moredun.ac.uk>
2022-09-30 12:08:30 +0100

    This is the bash script I have used to rebasecall the large number of fast5 files from this MinION run on a grey seal faecal sample.

---
+++ recursive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name="rebasecalling_fast5_files"
+#SBATCH --mail-user=karen.keegan@moredun.ac.uk
+#SBATCH --mail-type=END,FAIL
+#SBATCH --cpus-per-task=12
+echo "Input path is $1"
+echo "Save path is $2"
+
+
+
+guppy_basecaller --input_path $1 --save_path $2 --recursive --flowcell FLO-MIN106  --kit SQK-RAD004
+
