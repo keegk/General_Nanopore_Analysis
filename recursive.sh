e2f8306156356ec9c6d4a328b49b6e77d98b4901
Karen Keegan <karen.keegan@moredun.ac.uk>
2022-12-06 15:01:46 +0000

    changed the kit name to SQK-LSK109

--- recursive.sh
+++ recursive.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name="rebasecalling_fast5_files"
-#SBATCH --mail-user=karen.keegan@moredun.ac.uk
-#SBATCH --mail-type=END,FAIL
 #SBATCH --cpus-per-task=12
-echo "Input path is $1"
-echo "Save path is $2"
+export PATH=/home/kkeegan/projects/jhi/bioss/kkeegan_onttestdata/ont-guppy-cpu/bin:$PATH
 
 
-
-guppy_basecaller --input_path $1 --save_path $2 --recursive --flowcell FLO-MIN106  --kit SQK-RAD004
-
+guppy_basecaller --recursive --flowcell FLO-MIN106  --kit SQK-LSK109
