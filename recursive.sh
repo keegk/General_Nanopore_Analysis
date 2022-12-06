309eaca6bd97a581ddb06a26429b057472775f1e
Karen Keegan <karen.keegan@moredun.ac.uk>
2022-12-06 15:04:49 +0000

    UPDATED

--- recursive.sh
+++ recursive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name="rebasecalling_fast5_files"
+#SBATCH --mail-user=karen.keegan@moredun.ac.uk
+#SBATCH --mail-type=END,FAIL
 #SBATCH --cpus-per-task=12
-export PATH=/home/kkeegan/projects/jhi/bioss/kkeegan_onttestdata/ont-guppy-cpu/bin:$PATH
+echo "Input path is $1"
+echo "Save path is $2"
 
-
-guppy_basecaller --recursive --flowcell FLO-MIN106  --kit SQK-LSK109
 
 
 guppy_basecaller --input_path $1 --save_path $2 --recursive --flowcell FLO-MIN106  --kit SQK-LSK109
-
