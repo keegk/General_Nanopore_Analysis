e030b66281643b304a4569610bc1824b1d356553
Karen Keegan <karen.keegan@moredun.ac.uk>
2025-08-31 12:59:20 +0100

    blastn on CARD script

---
+++ blast_CARD_outfmt7_coverage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name="GOUGH_BLASTN_CARD"
+#SBATCH --mail-user=karen.keegan@moredun.ac.uk
+#SBATCH --mail-type=END,FAIL
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --partition=medium
+
+source activate /mnt/shared/scratch/kkeegan/apps/conda/envs/blast
+
+export BLASTDB=$BLASTDB:/mnt/shared/projects/jhi/bioss/kkeegan_onttestdata/blast_databases/CARD/CARDdb/
+
+
+blastn -query $1 -db CARD_db -out $2 -num_threads 12 -outfmt "7 sscinames qacc sacc pident qcovs qcovhsp length mismatch gapopen gaps slen qlen qstart qend sstart send evalue staxids bitscore"  
